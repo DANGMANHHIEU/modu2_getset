ef3841c08bddf8f2b8d70c53607c2c827bdb8790
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-03-28 11:28:54 +0700

    thuc hanh

---
+++ src/ex1/TestStaticMethod.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public class TestStaticMethod {
+    public static void main(String[] args) {
+       student.change();
+        student s1 = new student(111,"hoang");
+        student s2  = new student(222,"khanh");
+        student s3 =new student(333,"nam");
+
+        s1.display();;
+        s2.display();
+        s3.display();
+    }
+}
