a2383380162e3c067a1d3c25c3c9e8c2ea37001a
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-03-28 16:00:18 +0700

    bai2

---
+++ src/bai2/test.java
@@ -0,0 +1,10 @@
+package bai2;
+
+public class test {
+    public static void main(String[] args) {
+        student student = new student();
+        student.setName("a");
+        student.setClassname("c03");
+        System.out.println(student);
+    }
+}
