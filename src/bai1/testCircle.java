dac3a57c29211b431a063375854fa8861da487ec
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-03-28 15:34:37 +0700

    bai1

---
+++ src/bai1/testCircle.java
@@ -0,0 +1,9 @@
+package bai1;
+
+public class testCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        System.out.println("radius: "+ circle.getRadius());
+        System.out.println("Area: "+ circle.getArea());
+    }
+}
