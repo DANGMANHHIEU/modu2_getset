d4f7d0adda695221cfd84d96f0a297dd41d77c1f
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-03-28 15:59:34 +0700

    bai2

---
+++ src/bai2/student.java
@@ -0,0 +1,13 @@
+package bai2;
+
+public class student {
+    private String name = "John";
+    private String classname = "C02";
+    public student(){}
+     void setName(String n){
+        this.name = n;
+    }
+    void setClassname(String c){
+        this.classname = c;
+    }
+}
